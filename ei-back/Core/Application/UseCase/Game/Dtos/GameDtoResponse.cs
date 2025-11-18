c8a67d8de6c32061a54452cc5ce834c2432131dd
Wilknis <wilknisoliveira@gmail.com>
2025-11-17 22:50:36 -0300

    feat(game): return game status on the get api

--- ei-back/Core/Application/UseCase/Game/Dtos/GameDtoResponse.cs
+++ ei-back/Core/Application/UseCase/Game/Dtos/GameDtoResponse.cs
@@ -1,10 +1,12 @@
-﻿namespace ei_back.Core.Application.UseCase.Game.Dtos
+﻿using ei_back.Core.Domain.Enums;
+
+namespace ei_back.Core.Application.UseCase.Game.Dtos
 {
     public record GameDtoResponse
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public Guid OwnerUserId { get; set; }
-        public string SystemGame { get; set; }
+        public GameStatus GameStatus { get; set; }
     }
 }
